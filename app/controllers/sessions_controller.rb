d8965753b674d2b48f29019da3942c5003179dba
hickson1988 <hickson1988@hotmail.com>
2015-06-08 17:11:13 +0300

    authentication validation added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
+      flash[:error] = "Invalid email or password."
       redirect_to login_path
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, email_format: { message: "doesn't look like an email address" }
+  validates :password, length: { in: 6..20 }
 end
